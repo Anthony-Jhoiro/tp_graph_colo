6afb060d5deb48eb39fe014884f2ca9188402aa1
Anthony <quereantho@gmail.com>
2021-05-22 11:06:48 +0200

    Update file structure

--- main.c
+++ main.c
@@ -8,17 +8,19 @@ int main(int argc, char const *argv[])
 
     srand(time(NULL)); 
     graph myGraph = readGraph("./assets/1-FullIns_3.col");
-    graph_colo myGraphColo = createGraphColo(myGraph->size);
+    printGraph(myGraph);
 
-    myGraphColo->g = myGraph;
-    alg_genetique(myGraphColo, 100, 1000);
+    // graph_colo myGraphColo = createGraphColo(myGraph->size);
+
+    // myGraphColo->g = myGraph;
+    // alg_genetique(myGraphColo, 100, 1000);
 
 
     // colorColoredGraphWithColoredColors(myGraphColo);
 
-    printGraphColo(myGraphColo);
+    // printGraphColo(myGraphColo);
 
-    writePumlColo("res2.puml", myGraphColo);
+    // writePumlColo("res2.puml", myGraphColo);
 
     return 0;
 }
